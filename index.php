e1f303983266d201c345b78f5f32344c7f4378a5
GuilhermeFaustino <guilhermemendes.info@gmail.com>
2022-01-12 22:13:57 -0300

    Desenvolvimento do topo

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Imob BootStrap_Build</title>
+    <link rel="stylesheet" href="_cdn/css/bootstrap_person.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+<body>
+
+<header class="main_header">
+    <div class="header_bar bg-front">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center p-2 align-items-center text-white">
+                    <i class="icon-location-arrow"></i>
+                    <p class="my-auto ml-3">Avenida Pequeno Principe, 0 Campeche</br>Florianópolis/SC</p>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-clock-o"></i>
+                    <p class="my-auto ml-3">Segunda a Sexta: 09:00h as 18:00 </br>Sáb/Dom - Plantão/SC</p>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-envelope"></i>
+                    <p class="my-auto ml-3">guilhermemendes.dev@gmail.com</br>+55 (81) 99547-7407/SC</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-md navbar-light my-3">
+        <div class="container">
+
+                    <div class="navbar-brand">
+                        <a href="index.php">
+                            <h1 class="d-none">Imobiliaria</h1>
+                            <img src="assets/images/logo.png" class="d-inline-block" width="280">
+                        </a>
+                    </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active text-front" href="#">Destaque</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#">Alugar</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#">Comprar</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#">Contato</a></li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+    </nav>
+</header>
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
